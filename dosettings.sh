b956bfd38c557b95edda4e74cdd5a678bd3be1c4
Cihat Genç <cihat@catwithboots.com>
2016-06-14 21:33:37 +0200

    asdfdasf

--- dosettings.sh
+++ dosettings.sh
@@ -4,3 +4,5 @@ sed -i 's/localhost/$SCHEMA_REGISTRY/g' /usr/bin/kafka-avro-console-producer
 sed -i 's/8081/$SCHEMA_REGISTRY_PORT/g' /usr/bin/kafka-avro-console-producer
 sed -i 's/localhost/$SCHEMA_REGISTRY/g' /usr/bin/kafka-avro-console-consumer
 sed -i 's/8081/$SCHEMA_REGISTRY_PORT/g' /usr/bin/kafka-avro-console-consumer
+
+exec $0
